0c36fd3ce63530ebb3c6f04ec9af86b85f72a802
wagnerjanos18 <wagjan151@hengersor.hu>
2024-12-10 12:13:11 +0100

    Add files via upload

---
+++ node.js
@@ -0,0 +1,1 @@
+fetch('https://jsonplaceholder.typicode.com/posts'). then(res => res.json()).then(res => console.log(res))
---
+++ node2/index.js
@@ -0,0 +1,9 @@
+// wrapper function
+// function (__dirname, __filename, module, require, exports) {
+
+
+
+console.log(__dirname);
+console.log(__filename);
+
+// }
